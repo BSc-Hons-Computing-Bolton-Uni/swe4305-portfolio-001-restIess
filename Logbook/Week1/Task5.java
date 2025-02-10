4770a8c3f8be8950c539680b19aaad7eae34fc82
restIess <mark.thwaite@hotmail.com>
2025-02-10 10:57:15 +0000

    Task 1: Within the Main.java file, add a line of code within the main method that will output the text "Hello World!" to the screen. Compile and execute the program to check that it works as required.
    Task 2 - Now amend Main.java to enable the program to enter a user's name and store in a variable.
    Change the output of "Hello World!" so that the program outputs a personal greeting to the name
    entered (for example "Hello Simon!")
    
    Task 3 - Design a program that asks the user to enter values for the length and height of a rectangle and
    then outputs the perimeter and area of the rectangle to the screen
    
    Task 4 - Write a program that calculates and displays the average of two numbers entered. Check this
    program works for whole and decimal values
    
    Task 5 - Write a program that allows the user to enter two integer values. The program should then output the original order they were entered, then output the values in reverse order (swapped)

--- Logbook/Week1/Task5.java
+++ Logbook/Week1/Task5.java
@@ -1,4 +1,24 @@
 package Logbook.Week1;
 
+import java.util.Scanner;
+
 public class Task5 {
+    public static void main(String[] args) {
+        // create a scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Asks the user to enter the first number
+        System.out.print("Enter the first integer: ");
+        int num1 = scanner.nextInt();
+
+        // Asks the user to enter the second number
+        System.out.print("Enter the second integer: ");
+        int num2 = scanner.nextInt();
+
+        // Outputs the original order of the two numbers
+        System.out.println("Original order: " + num1 + num2);
+
+        // Outputs the reversed order
+        System.out.println("Reversed order: " + num2 + num1);
+    }
 }
