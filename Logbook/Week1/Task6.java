8e999aab6daa635851929b72b5b45d4b053a8e25
restIess <mark.thwaite@hotmail.com>
2025-02-10 11:44:40 +0000

    Task 1: Within the Main.java file, add a line of code within the main method that will output the text "Hello World!" to the screen. Compile and execute the program to check that it works as required.
    Task 2 - Now amend Main.java to enable the program to enter a user's name and store in a variable.
    Change the output of "Hello World!" so that the program outputs a personal greeting to the name
    entered (for example "Hello Simon!")
    
    Task 3 - Design a program that asks the user to enter values for the length and height of a rectangle and
    then outputs the perimeter and area of the rectangle to the screen
    
    Task 4 - Write a program that calculates and displays the average of two numbers entered. Check this
    program works for whole and decimal values
    
    Task 5 - Write a program that allows the user to enter two integer values. The program should then output the original order they were entered, then output the values in reverse order (swapped)
    
    Task 6 - Design a program that allows a user to type in their age. The program should then work out which
    year they were born and output this to the screen.

--- Logbook/Week1/Task6.java
+++ Logbook/Week1/Task6.java
@@ -1,4 +1,46 @@
 package Logbook.Week1;
 
+import java.util.Scanner;
+import java.time.LocalDate;
+
+// Note: Realised my program could be wrong, as it's only removing the age input
+// from the current year, so I've decided to add the date and month too so an accurate
+// year can be output.
+
+import java.util.Scanner;
+import java.time.LocalDate;
+
 public class Task6 {
+    public static void main(String[] args) {
+        // create a scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Asks the user to enter their age
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        // Gets the current date that the computer's/device's using
+        LocalDate currentDate = LocalDate.now();
+
+        // Calculates the birth year based on the current year
+        int birthYear = currentDate.getYear() - age;
+
+        // This asks the user to enter their birthdate (to check if they've had their birthday yet)
+        System.out.print("Enter your birth month (MM): ");
+        int birthMonth = scanner.nextInt();
+
+        System.out.print("Enter your birth day (DD): ");
+        int birthDay = scanner.nextInt();
+
+        // Calculate the user's birthdate this year
+        LocalDate birthDateThisYear = LocalDate.of(currentDate.getYear(), birthMonth, birthDay);
+
+        // If the user's birthday hasnt happened yet this year, - the birth year by 1
+        if (currentDate.isBefore(birthDateThisYear)) {
+            birthYear = currentDate.getYear() - age - 1;
+        }
+
+        // Outputs birth year
+        System.out.println("You were born in: " + birthYear);
+    }
 }
