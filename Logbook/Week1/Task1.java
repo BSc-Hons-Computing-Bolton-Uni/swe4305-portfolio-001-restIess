160c10651af9d44f142bc26e21aaaf2f761d5b47
restIess <mark.thwaite@hotmail.com>
2025-02-10 10:37:09 +0000

    Task 1: Within the Main.java file, add a line of code within the main method that will output the text "Hello World!" to the screen. Compile and execute the program to check that it works as required. Commit and push your changes to Main.java to your repository.

--- Logbook/Week1/Task1.java
+++ Logbook/Week1/Task1.java
@@ -1,6 +1,6 @@
 package Logbook.Week1;
 
-public class Main {
+public class Task1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
---
+++ Logbook/Week1/Task2.java
@@ -0,0 +1,17 @@
+package Logbook.Week1;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        // Create a scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Asks the user to enter their name
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Output with the user's name
+        System.out.println("Hello " + name + "!");
+    }
+}
