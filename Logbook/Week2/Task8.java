74b4b952fa3edcef4766ba5d02e5ec6f1a0a1894
restIess <brollama1234567@gmail.com>
2025-02-23 15:55:00 +0000

    Week 2 - task 8 - Week 2 complete

--- Logbook/Week2/Task8.java
+++ Logbook/Week2/Task8.java
@@ -1,4 +1,13 @@
 package Logbook.Week2;
 
 public class Task8 {
+    public static void main(String[] args) {
+        // prints even numbers from 1 to 20
+        System.out.println("Even numbers from 1 to 20:");
+        for (int i = 1; i <= 10; i++) {
+            int evenNumber = i * 2;
+            System.out.print(evenNumber + " ");
+        }
+        System.out.println();
+    }
 }
