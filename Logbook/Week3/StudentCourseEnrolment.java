aafe152a6c3badf033ee0c50a0932b07886c130a
restIess <brollama1234567@gmail.com>
2025-02-23 16:08:48 +0000

    Task 3 - Defining a print method that will output the values for these ID and name attributes.

--- Logbook/Week3/StudentCourseEnrolment.java
+++ Logbook/Week3/StudentCourseEnrolment.java
@@ -1,15 +1,21 @@
 package Logbook.Week3;
-
+// task 1
 public class StudentCourseEnrolment {
     // 'Student' class with attributes ID and name
     static class Student {
         int id;
         String name;
-
+// task 2
         // constructor to initialise the ID and name
         public Student(int id, String name) {
             this.id = id;
             this.name = name;
+        }
+// task 3
+        // display students' details w/ print method
+        public void print() {
+            System.out.println("Student ID: " + id);
+            System.out.println("Student Name: " + name);
         }
     }
 }
