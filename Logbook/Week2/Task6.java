b3ab0e231b8bc7ddaea351181e7c8aa770b90bd3
restIess <brollama1234567@gmail.com>
2025-02-23 15:43:28 +0000

    Week 2 - task 6

--- Logbook/Week2/Task6.java
+++ Logbook/Week2/Task6.java
@@ -1,4 +1,30 @@
 package Logbook.Week2;
+import java.util.Scanner;
 
 public class Task6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean repeat;
+
+        do {
+            // asks user to choose a multiplication table
+            System.out.print("Enter a number to see its multiplication table: ");
+            int num = scanner.nextInt();
+
+            // prints multiplication table for the chosen number
+            System.out.println("Multiplication table for " + num + ":");
+            for (int i = 1; i <= 12; i++) {
+                System.out.println(i + " x " + num + " = " + (i * num));
+            }
+
+            // asks if user wants to try another table
+            System.out.print("Do you want to try another table? (yes/no): ");
+            String response = scanner.next();
+            repeat = response.equalsIgnoreCase("yes");
+
+        } while (repeat);
+
+        System.out.println("Goodbye!");
+        scanner.close();
+    }
 }
